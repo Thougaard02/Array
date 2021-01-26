9e220a06dc4fa923c5b3818c151ea599f4a971c8
Rasmus <Rasmus@vinperlen.dk>
2021-01-26 08:48:04 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(array[4]);
+
+            array[4] = 10;
+
+            Console.WriteLine(array[4]);
+
+            Console.ReadKey();
+        }
+    }
+}
