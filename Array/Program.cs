566ae8b3b49de967625fef4183307cd84605fbe6
Rasmus <Rasmus@vinperlen.dk>
2021-01-26 08:48:47 +0100

    commit

--- Array/Program.cs
+++ Array/Program.cs
@@ -10,7 +10,6 @@ namespace Array
     {
         static void Main(string[] args)
         {
-
             int[] array = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             Console.WriteLine(array[4]);
